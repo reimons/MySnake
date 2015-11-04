269b82601ddce3822d4b26fbce69d88cb2ebc4f6
reimons <reimons07@mail.ru>
2015-11-04 22:32:08 +0200

    Процедурный подход. Функция Drow

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,8 +10,21 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            int x1 = 3;
+            int y1 = 1;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+            int x2 = 7;
+            int y2 = 4;
+            char sym2 = '#';
+            Draw(x2, y1, sym2);
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition( x , y );
+            Console.Write(sym);
         }
     }
 }
